944d60012e56fcfff1df0ec5a1fa014a26b717cc
Anthony de Melo <anthonymelo377@gmail.com>
2025-09-18 21:05:44 -0300

    Exercicios

---
+++ src/aula006/Ex1.java
@@ -0,0 +1,13 @@
+package aula006;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        float nota = 70;
+        if(nota >= 60){
+            System.out.println("Aluno aprovado");
+        }
+        else{
+            System.out.println("Aluno reprovado");
+        }
+    }
+}
